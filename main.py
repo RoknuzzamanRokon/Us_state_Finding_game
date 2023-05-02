8bd3e5bc09b429b4f51334dee6398a0404954fa0
Rokon Ron <rokon.raz@gmail.com>
2023-05-02 08:54:37 +0600

    Add scerch button.

--- main.py
+++ main.py
@@ -1,18 +1,18 @@
 import turtle
 
+
 screen = turtle.Screen()
 screen.title("U.S states game")
 image = "blank_states_img.gif"
 screen.addshape(image)
 turtle.shape(image)
 
+display = screen.textinput(title="us states find game.", prompt="Right a us-stat name.")
 
-def click_on_display(x, y):
-    print(x, y)
-
-
-turtle.onscreenclick(click_on_display)
-
+# def click_on_display(x, y):
+#     print(x, y)
+#
+# turtle.onscreenclick(click_on_display)
 
 screen.mainloop()
 
