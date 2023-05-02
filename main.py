e1467833ae82ea5af9e24f0ba051b539c1db1588
Rokon Ron <rokon.raz@gmail.com>
2023-05-02 16:01:32 +0600

    Encrement number and count it.

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import turtle
+import pandas as pd
 
 
 screen = turtle.Screen()
@@ -7,12 +8,32 @@ image = "blank_states_img.gif"
 screen.addshape(image)
 turtle.shape(image)
 
-display = screen.textinput(title="us states find game.", prompt="Right a us-stat name.")
+read_csv_file = pd.read_csv('50_states.csv')
+all_state = read_csv_file.state.to_list()
 
-# def click_on_display(x, y):
-#     print(x, y)
-#
-# turtle.onscreenclick(click_on_display)
+make_list = 0
 
-screen.mainloop()
 
+while make_list < 50:
+    display = screen.textinput(title=f"Your score:{make_list}/50", prompt="wright a us-stat name.").capitalize()
+
+    if display in all_state:
+        make_list += 1
+        print(make_list)
+        read_state = read_csv_file[read_csv_file['state'] == display]
+        make_dict = read_state.to_dict()
+        position = (list(make_dict['x'].values())[0], list(make_dict['y'].values())[0])
+        name = (list(make_dict['state'].values())[0])
+
+        t = turtle.Turtle()
+        t.hideturtle()
+        t.penup()
+        t.goto(position)
+        t.write(name, align="center", font=('Arial', 10, 'normal'))
+    # def click_on_display(x, y):
+    #     print(x, y)
+    #
+    # turtle.onscreenclick(click_on_display)
+
+# screen.mainloop()
+screen.exitonclick()
